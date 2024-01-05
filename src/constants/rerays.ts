a9bf9d6443197344f36f3193d08a0287814198b4
ocknamo <s14pes@gmail.com>
2024-01-06 00:18:21 +0900

    feat: update default relays

--- src/constants/rerays.ts
+++ src/constants/rerays.ts
@@ -1,11 +1,10 @@
-// TODO: update
 export const defaultRelays: string[] = [
 	'wss://relay.damus.io',
 	'wss://relay-jp.nostr.wirednet.jp',
 	'wss://nostr-relay.nokotaro.com',
-	'wss://nostr.fediverse.jp',
 	'wss://nostr.holybea.com',
 	'wss://nos.lol',
 	'wss://relay.snort.social',
-	'wss://nostr.mom',
+	'wss://relay.nostr.band',
+	'wss://purplepag.es',
 ];
