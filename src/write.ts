a709c5c6d5a0a21681e3d3d49cddd31f8448ab52
ocknamo <s14pes@gmail.com>
2024-01-23 01:06:00 +0900

    fix: Connection timeout implemented

--- src/write.ts
+++ src/write.ts
@@ -1,12 +1,12 @@
 import { Event, Relay, relayInit } from 'nostr-tools';
 
-export type PostResult = { result: string; connection: Relay };
+export type PostResult = { result: string; connection?: Relay };
 
 export async function oneTimePostToMultiRelay(
 	event: Event,
 	relayUris: string[],
 	timeoutMs = 10000,
-	connections: Relay[] = [],
+	connections: (Relay | undefined)[] = [],
 ): Promise<PostResult[]> {
 	const promises: Promise<PostResult>[] = [];
 	relayUris.forEach((uri, i) => {
@@ -26,15 +26,35 @@ export async function oneTimePost(
 	if (connection) {
 		relay = connection;
 	} else {
+		/**
+		 * set connection
+		 */
 		relay = relayInit(relayUri);
-		relay.on('connect', () => {
-			console.log(`connected to ${relay.url}`);
-		});
-		relay.on('error', () => {
-			console.log(`failed to connect to ${relay.url}`);
+		const connectionPromise: Promise<string> = new Promise((resolve, reject) => {
+			relay.on('connect', () => {
+				console.log(`connected to ${relay.url}`);
+				resolve(`[success to connect]: ${relay.url}`);
+			});
+			relay.on('error', () => {
+				console.log(`failed to connect to ${relay.url}`);
+				reject(`[failed to connect]: ${relay.url}`);
+			});
+
+			timeout = setTimeout(() => {
+				relay.close();
+				reject(`[timeout]: ${relay.url}`);
+			}, timeoutMs);
 		});
 
-		await relay.connect();
+		relay.connect().catch((r) => {
+			const reason = r ? `[${r}]: ${relay.url}` : `[failed to connect]: ${relay.url}`;
+
+			return { reason, connection: undefined };
+		});
+
+		await connectionPromise.catch((r) => {
+			return { r, connection: undefined };
+		});
 	}
 
 	/**
@@ -47,7 +67,7 @@ export async function oneTimePost(
 	 */
 	let pub = relay.publish(event);
 	let timeout: NodeJS.Timeout | null = null;
-	const promise: Promise<string> = new Promise((resolve, reject) => {
+	const publishPromise: Promise<string> = new Promise((resolve, reject) => {
 		pub.on('ok', () => {
 			console.log(`${relay.url} has accepted our event`);
 			resolve(`[accepted]: ${relay.url}`);
@@ -58,6 +78,7 @@ export async function oneTimePost(
 		});
 
 		timeout = setTimeout(() => {
+			relay.close();
 			reject(`[timeout]: ${relay.url}`);
 		}, timeoutMs);
 	});
@@ -65,11 +86,19 @@ export async function oneTimePost(
 	/**
 	 * Get result.
 	 */
-	await promise.then((r) => (result = r)).catch((r) => (result = r));
+	let resultRelay = undefined;
+	await publishPromise
+		.then((r) => {
+			result = r;
+			resultRelay = relay;
+		})
+		.catch((r) => {
+			result = r;
+		});
 
 	if (!!timeout) {
 		clearTimeout(timeout);
 	}
 
-	return { result, connection: relay };
+	return { result, connection: resultRelay };
 }
