4cae8d696c55b2bcbc72fcfaecb425e51f3b0f32
ocknamo <s14pes@gmail.com>
2024-01-23 01:37:33 +0900

    chore: update relays (#17)

--- src/constants/rerays.ts
+++ src/constants/rerays.ts
@@ -2,9 +2,8 @@ export const defaultRelays: string[] = [
 	'wss://relay.damus.io',
 	'wss://relay-jp.nostr.wirednet.jp',
 	'wss://nostr-relay.nokotaro.com',
-	'wss://nostr.holybea.com',
-	'wss://nos.lol',
-	'wss://relay.snort.social',
-	'wss://relay.nostr.band',
-	'wss://purplepag.es',
+	'wss://bitcoiner.social',
+	'wss://relay.primal.net',
+	'wss://nostr-01.yakihonne.com',
+	'wss://nostr-02.yakihonne.com',
 ];
