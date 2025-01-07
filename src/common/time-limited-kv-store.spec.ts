797eed4c0d9e9d261a423751f2bf88ab901d7847
ocknamo <s14pes@gmail.com>
2025-01-08 02:00:38 +0900

    refactor: kv store

--- src/common/time-limited-kv-store.spec.ts
+++ src/common/time-limited-kv-store.spec.ts
@@ -1,36 +1,40 @@
-import { sleep } from '../test/utils';
 import { TimeLimitedKvStore } from './time-limited-kv-store';
 import { Event } from 'nostr-tools';
 
 describe('src/common/time-limited-ly-store.ts', () => {
-	let store: TimeLimitedKvStore;
+	let store: TimeLimitedKvStore<Partial<Event>>;
+
+	beforeAll(() => {
+		jest.useFakeTimers();
+	});
+
 	beforeEach(() => {
 		store = new TimeLimitedKvStore();
 	});
 
 	it('should set store', () => {
-		store.set({ id: 'mock_id_1' } as Event, Date.now() + 1000);
+		store.set('mock_id_1', { id: 'mock_id_1' } as Event, Date.now() + 1000);
 
 		expect(store.has('mock_id_1')).toBeTruthy();
 	});
 
 	it('should expierd id is not stored', () => {
-		store.set({ id: 'mock_id_2' } as Event, Date.now() - 1);
+		store.set('mock_id_2', { id: 'mock_id_2' } as Event, Date.now() - 1);
 
 		expect(store.has('mock_id_2')).toBeFalsy();
 	});
 
 	it('should expierd id is deleted and not are stored', async () => {
-		store.set({ id: 'mock_id_1' } as Event, Date.now() - 1);
-		store.set({ id: 'mock_id_2' } as Event, Date.now() - 10);
-		store.set({ id: 'mock_id_3' } as Event, Date.now() - 100);
-		store.set({ id: 'mock_id_4' } as Event, Date.now() + 1);
-		store.set({ id: 'mock_id_5' } as Event, Date.now() + 1 * 1000);
-		store.set({ id: 'mock_id_6' } as Event, Date.now() + 2 * 1000);
-		store.set({ id: 'mock_id_7' } as Event, Date.now() + 10 * 1000);
-		store.set({ id: 'mock_id_8' } as Event, Date.now() + 100 * 1000);
+		store.set('mock_id_1', { id: 'mock_id_1' } as Event, Date.now() - 1);
+		store.set('mock_id_2', { id: 'mock_id_2' } as Event, Date.now() - 10);
+		store.set('mock_id_3', { id: 'mock_id_3' } as Event, Date.now() - 100);
+		store.set('mock_id_4', { id: 'mock_id_4' } as Event, Date.now() + 1);
+		store.set('mock_id_5', { id: 'mock_id_5' } as Event, Date.now() + 1 * 1000);
+		store.set('mock_id_6', { id: 'mock_id_6' } as Event, Date.now() + 2 * 1000);
+		store.set('mock_id_7', { id: 'mock_id_7' } as Event, Date.now() + 10 * 1000);
+		store.set('mock_id_8', { id: 'mock_id_8' } as Event, Date.now() + 100 * 1000);
 
-		await sleep(500);
+		jest.advanceTimersByTime(500);
 
 		expect(store.has('mock_id_1')).toBeFalsy();
 		expect(store.has('mock_id_2')).toBeFalsy();
--- src/common/time-limited-kv-store.ts
+++ src/common/time-limited-kv-store.ts
@@ -1,35 +1,38 @@
-import { Event } from 'nostr-tools';
-
 /**
  * This is a simple Store service that stores IDs with a time limit.
  * Suitable for frequently referenced use cases as expired IDs are removed when referenced.
  */
-export class TimeLimitedKvStore {
+export class TimeLimitedKvStore<T> {
 	// NOTE: The period is unixtime in milliseconds.
-	// private idPeriodMap: Map<string, number>;
-	private idEventMap: Map<string, Partial<Event> & { period: number }>;
+	private keyValueMap: Map<string, { value: T; period: number }>;
 	constructor() {
-		// this.idPeriodMap = new Map();
-		this.idEventMap = new Map();
+		this.keyValueMap = new Map();
 	}
 
-	set(event: Event, period: number): void {
-		this.idEventMap.set(event.id, { ...event, period });
+	set(key: string, value: T, period: number): void {
+		this.keyValueMap.set(key, { value, period });
 	}
 
-	has(id: string): boolean {
-		this.clearExpierdId();
-		return this.idEventMap.has(id);
+	has(key: string): boolean {
+		this.clearExpierd();
+		return this.keyValueMap.has(key);
 	}
 
 	count(): number {
-		return this.idEventMap.size;
+		return this.keyValueMap.size;
 	}
 
-	clearExpierdId(): void {
+	values(): {
+		value: T;
+		period: number;
+	}[] {
+		return Array.from(this.keyValueMap.values());
+	}
+
+	clearExpierd(): void {
 		const now = Date.now();
 
-		this.idEventMap.forEach((v, k, m) => {
+		this.keyValueMap.forEach((v, k, m) => {
 			if (v.period < now) {
 				m.delete(k);
 			}
