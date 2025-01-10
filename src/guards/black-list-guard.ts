32638752c74153f59c569bf7fddf6d3b97320d28
ocknamo <s14pes@gmail.com>
2025-01-11 01:23:03 +0900

    fix: correct bugs of list guards

--- src/guards/black-list-guard.ts
+++ src/guards/black-list-guard.ts
@@ -1,7 +1,11 @@
 import { Event } from 'nostr-tools';
 import { getHexPubKey } from '../convert/get-hex';
 
-export function blackListGuard(event: Event, npubs: string[]): boolean {
+export function blackListGuard(event: Event, npubs: string[] = []): boolean {
+	if (npubs.length === 0) {
+		return true;
+	}
+
 	return !npubs.some((npub) => {
 		return event.pubkey.toUpperCase() === getHexPubKey(npub).toUpperCase();
 	});
